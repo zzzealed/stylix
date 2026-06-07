cc50e5347f15f1bcc47f7aa594b35e6212e7b5af
Zeal <zzzealed@proton.me>
2026-06-07 06:30:57 +0200

    add Glide homepage and description

--- modules/firefox/meta.nix
+++ modules/firefox/meta.nix
@@ -5,6 +5,7 @@
     Firefox = "http://www.mozilla.com/en-US/firefox";
     Floorp = "https://floorp.app";
     LibreWolf = "https://librewolf.net";
+    Glide = "https://glide-browser.app";
   };
   maintainers = with lib.maintainers; [
     Flameopathic
@@ -13,8 +14,8 @@
   ];
   description = ''
     This module supports [Firefox](https://www.mozilla.org/firefox), in addition
-    to [Floorp](https://floorp.app) and [LibreWolf](https://librewolf.net),
-    which are Firefox derivatives.
+    to [Floorp](https://floorp.app), [LibreWolf](https://librewolf.net), and
+    [Glide](https://glide-browser.app), which are Firefox derivatives.
 
     The same implementation is shared between all of these browsers, but they don't
     share option values.
